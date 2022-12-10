257e6ebd7bbab82051feca3b3a710f18d9cf295c
Marcel Leyva <leyva.marccel@gmail.com>
2022-12-10 02:37:10 +0100

    v

--- Components/button.js
+++ Components/button.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './button.module.css'
 
 export default function Button() {
   return (
     <>
-        <button className='bg-indigo-500 text-gray-200 font-medium rounded-md mb-5 mt-3 w-full py-2'>Enviar</button>
+        <button className={styles.button}>Enviar</button>
     </>
   )
 }
